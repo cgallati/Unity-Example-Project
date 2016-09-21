1b0591f3e15dc924fa742b17e475e92f58461497
Chad Gallati <chadgallati@gmail.com>
2016-09-21 15:08:43 -0400

    Completed tutorial 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Called before any physics calculations 
+	private Rigidbody rb;
+	public float speed;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVeritcal = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVeritcal);
+
+		rb.AddForce (movement * speed);
+	}
+}
