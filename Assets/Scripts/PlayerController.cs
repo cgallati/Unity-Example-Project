c214b6c71ede6ce30260329c6466ddce1b2bcc01
Chad Gallati <chadgallati@gmail.com>
2016-09-21 21:23:12 -0400

    Finished
    Should have committed more frequently, but it’s done.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -6,9 +7,16 @@ public class PlayerController : MonoBehaviour {
 	//Called before any physics calculations 
 	private Rigidbody rb;
 	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -16,8 +24,24 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVeritcal = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVeritcal);
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVeritcal);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
 	}
 }
